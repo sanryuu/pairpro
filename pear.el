951fa0b8af3078c27a5a32f696348a265ea18361
sanryuu <cpu.cpu.cpu9@gmail.com>
2014-12-18 01:22:42 +0900

    pearpro-modeのマイナーモードの作成

--- pear.el
+++ pear.el
@@ -1,3 +1,4 @@
+(defvar pearpro-mode nil)
 
 (defvar pp-command-emacs "/Applications/Emacs.app/Contents/MacOS/Emacs")
 
@@ -7,4 +8,15 @@
   (interactive)
   (shell-command (concat pp-command-emacs " -Q -l " pp-pear-setting-file " &")))
 
+(define-minor-mode pearpro-mode
+  "pearpro-mode"
+  :lighter " pp" ; for mode line
+  (if pearpro-mode
+      (pearpro-mode-start)
+    (pearpro-mode-end)))
 
+(defun pearpro-mode-start ()
+  )
+
+(defun pearpro-mode-end ()
+  )
